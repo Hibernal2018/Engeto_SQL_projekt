00ee6197f8fa812ce004522cb1a593e9ad56dd10
Milan Novák <novakmilan@email.cz>
2024-11-11 09:58:48 +0100

    Oprava dle poznamek lektora

--- 01_vyvoj_mezd_podle_odvetvi.sql
+++ 01_vyvoj_mezd_podle_odvetvi.sql
@@ -2,29 +2,7 @@
  * Otázka č.1 - Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 */
 
-WITH yearly_wages_industry AS
-	(
-	SELECT 
-		industry_branch_code,
-		branch_name,
-		value_type_code,
-		value_name,
-		calculation_code,
-		calculation_name,
-		payroll_year,
-		avg_wages,
-		ROUND(AVG(avg_wages), 2) AS avg_wages_per_year
-	FROM 
-		t_milan_novak_project_sql_primary_final AS tmnpspf 
-	WHERE 
-		payroll_year < 2021
-		AND 
-		industry_branch_code IS NOT NULL
-	GROUP BY 
-		industry_branch_code,
-		payroll_year
-	)
-SELECT 
+SELECT
 	industry_branch_code,
 	branch_name,
 	value_type_code,
@@ -32,18 +10,28 @@ SELECT
 	calculation_code,
 	calculation_name,
 	payroll_year,
-	avg_wages_per_year,
+	avg_wages,
 	ROUND(
 			(
-				avg_wages_per_year - LAG(avg_wages_per_year) OVER
+				avg_wages - LAG(avg_wages) OVER
 				(PARTITION BY industry_branch_code ORDER BY payroll_year)
 			) 
-				/ LAG(avg_wages_per_year) OVER 
+				/ LAG(avg_wages) OVER 
 				(PARTITION BY industry_branch_code ORDER BY payroll_year)
 				* 100, 2
 		  ) AS wage_diff_per_cent 
 FROM 
-	yearly_wages_industry
+	t_milan_novak_project_sql_primary_final as tmnpspf
+WHERE 
+	payroll_year < 2021
+	AND 
+	industry_branch_code IS NOT NULL
 GROUP BY 
 	industry_branch_code,
-	payroll_year;
+	payroll_year,
+	avg_wages,
+	branch_name,
+	value_type_code,
+	value_name,
+	calculation_code,
+	calculation_name;
